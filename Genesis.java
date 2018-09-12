4f16fe8a2ec979cb93ae62b3fe957abc3336f0f0
Sonali Singh <singh.sonali68@gmail.com>
2018-09-12 19:04:47 +0530

    final

--- Genesis.java
+++ Genesis.java
@@ -2,13 +2,21 @@ package posist;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class Genesis {
 	
 	
-	// 1. creation of genesis node
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+	
+	// [ 1.] creation of genesis node
 	private class GenesisNode {
 		int ownerId=0;
 		float value=0;
@@ -22,12 +30,12 @@ public class Genesis {
 		String genesisNodeId="";
 		String hashValue="";
 		
-		// 2. set of child nodes of particular node
+		// [ 2.] set of child nodes of particular node
 		ArrayList<GenesisNode> children = new ArrayList<>();
 	}
 	
 	GenesisNode node=null;
-	
+	HashMap<Float, String> dat=new HashMap<>();
 
 	private GenesisNode root;
 	int size;
@@ -50,6 +58,12 @@ public class Genesis {
 
 		// get data
 		float data = scn.nextFloat();
+		
+		// [ 4. ] encrypt and decrypt data inside node
+		
+	
+		String hash1 = hashfun1(data);
+		dat.put(data,hash1);
 
 		// create node and increase size
 		GenesisNode child = new GenesisNode();
@@ -59,7 +73,7 @@ public class Genesis {
 		child.value = data;
 		child.nodenum++;
 
-		// 3. creating the child nodes originating from a particular node
+		// [ 3.] creating the child nodes originating from a particular node
 		System.out.println("enter the number of children for " + child.data);
 		int numGC = scn.nextInt();
 
@@ -71,6 +85,52 @@ public class Genesis {
 		// return
 		return child;
 	}
+	
+	
+	public String hashfun1(float value){
+		user u = new user();
+		String str1 = u.id+u.name+value;
+		return str1;
+		
+	}
+	
+	// [ 5.] verify user with encryption key
+	HashMap<String, String> User = new HashMap<>();
+	private class user{
+		String name="";
+		int id=0;
+		String pw="";
+		
+		public void add(){
+			
+			Scanner scn = new Scanner(System.in);
+			name=scn.nextLine();
+			pw = scn.nextLine();
+			User.put(name, pw);
+			
+		}
+		
+		public boolean chkpw(String p){
+			return p==pw;
+		}
+		
+		// [ 5.] check integrity of data
+		
+		public boolean chkdat(int val,String h){
+			String str=name+val+id;
+			String hs = dat.get(val);
+			
+			if(hs.equals(str)){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		
+	}
+	
+	
 
 
 
